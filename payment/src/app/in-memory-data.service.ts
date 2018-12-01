eb9b2dacfe91ccac3f642d916000e5112890526a
John Kocer <john.kocer@gmail.com>
2018-12-01 16:03:40 -0700

    Updated in-memory-data-service

--- payment/src/app/in-memory-data.service.ts
+++ payment/src/app/in-memory-data.service.ts
@@ -3,15 +3,15 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const members = [
-      { memberId: 1, fullName: 'Ms. Nice', zipCode:'84223', errorMessage:"OK", isSuccess:true },
-      { memberId: 2, fullName: 'John Smart', zipCode:'89223', errorMessage:"OK", isSuccess:true }
+      { id: 1, fullName: 'Ms. Nice', zipCode: '84223', errorMessage: "OK", isSuccess: true },
+      { id: 2, fullName: 'John Smart', zipCode: '89223', errorMessage: "OK", isSuccess: true }
     ];
 
     const products = [
-      { productId: 100, price: 100, name:'Reqular Montly'},
-      { productId: 101, price: 150, name:'Silver Montly'},
-      { productId: 103, price: 150, name:'Gold Montly'}
+      { id: 100, price: 100, name: 'Reqular Montly' },
+      { id: 101, price: 150, name: 'Silver Montly' },
+      { id: 103, price: 150, name: 'Gold Montly' }
     ];
-    return {members, products};
+    return { members, products };
   }
 }
